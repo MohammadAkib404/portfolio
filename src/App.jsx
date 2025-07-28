aba7cf1a228cbb0386c0b7e34e82824f29c58961
Mohammad Akib <akibo7394@gmail.com>
2025-07-28 20:54:55 +0530

    Integrated React Routing in the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
+
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Home from './Pages/Home'
+import About from './Pages/About'
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      
     </>
   )
 }
