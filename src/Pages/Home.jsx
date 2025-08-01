5ad357e4263b909dc22efa9aa322b23305142ddb
Mohammad Akib <akibo7394@gmail.com>
2025-08-01 20:46:03 +0530

    Modified Hero Section and Added Skill Display

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
     return (
-        <section className='text-white bg-ambient flex justify-center'>
+        <section className='text-white bg-ambient flex flex-col items-center'>
             <div className='bg-tint mt-10 z-10 flex flex-col max-w-6xl h-full justify-center items-center p-10'>
                 <h1 className='flex flex-col items-center mb-8'>
                     <strong className='text-8xl font-bold'>Frontend</strong>
@@ -12,6 +12,20 @@ function Home() {
                 <div>
                     <button className='bg-sky-400 mr-5 text-black px-15 py-5 font-semibold text-xl rounded-2xl'>View Projects →</button>
                     <button className='px-15 py-5 text-xl font-semibold rounded-2xl border border-sky-400'>Get In Touch</button>
+                </div>
+            </div>
+            <div className='flex justify-center text-center gap-24 p-8 w-full border-t border-slate-700/70'>
+                <div>
+                    <h4 className='text-5xl font-bold text-sky-400'>50+</h4>
+                    <p className='text-xl text-gray-300/80'>Projects</p>
+                </div>
+                <div>
+                    <h4 className='text-5xl font-bold text-sky-400'>3+</h4>
+                    <p className='text-xl text-gray-300/80'>Years Experience</p>
+                </div>
+                <div>
+                    <h4 className='text-5xl font-bold text-sky-400'>24h</h4>
+                    <p className='text-xl text-gray-300/80'>Response Time</p>
                 </div>
             </div>
         </section>
