3a15a70d5a9ce620d11fac2895dbe8b46f69ea21
Mohammad Akib <akibo7394@gmail.com>
2026-01-07 22:06:48 +0530

    Refactor Header Component with Navigation and Responsive Design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,81 @@
-import React from 'react'
-import { Heart } from 'lucide-react'
+import { CodeSquare, Contact, Home, Mail, Menu, X, } from "lucide-react";
+import React, { useState } from "react";
+import { siGithub } from "simple-icons/icons";
 
-function Header() {
+const navItems = [
+    { id: "home", label: "Home", icon: <Home className="size-6" /> },
+    { id: "about", label: "About", icon: <Contact className="size-6" /> },
+    { id: "projects", label: "Projects", icon: <CodeSquare className="size-6" /> },
+    { id: "contact", label: "Contact", icon: <Mail className="size-6" /> },
+];
+
+export default function Header({ active }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const scrollToSection = (id) => {
+        document.getElementById(id).scrollIntoView({ behavior: "smooth" })
+    }
+
     return (
-        <header className='fixed w-full px-16 py-6 text-xl flex justify-between  text-gray-300/80'>
-            <div className='flex gap-4 items-center'>
-                <div className='h-10 w-10 rounded-xl bg-sky-400 flex justify-center items-center'>
-                    <p className='w-5 h-5 rounded-full bg-black'></p>
+        <header className="bg-bg-light w-full fixed shadow-msm">
+            <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center ">
+                <div>
+                    <h1 className="font-logo text-4xl tracking-wider">Akif</h1>
                 </div>
-                <h3 className='text-white font-bold text-2xl'>Md Akib</h3>
+                <nav className="gap-3 hidden sm:flex">
+                    {navItems.map(item => (
+                        <div className={`flex gap-1.5 items-center px-4 py-2  rounded-xl text-sm ${active === item.id ? "bg-bg-dark font-semibold shadow-msm scale-105" : "bg-bg-light"} transition-all`}
+                            onClick={() => scrollToSection(item.id)}
+                            key={item.id}
+                        >
+                            {item.icon} <span>{item.label}</span>
+                        </div>
+                    ))}
+                </nav>
+                <div className="hidden sm:flex items-center gap-2">
+                    <button className="hover:shadow-msm p-1 transition-all duration-300 rounded-full">
+                        <span
+                            className="size-6 inline-block"
+                            dangerouslySetInnerHTML={{ __html: siGithub.svg }}
+                        />
+                    </button>
+                    <button className="hover:shadow-msm p-1 pb-2 transition-all duration-300 rounded-full">
+                        <img className="size-6" src="./linkedin.svg" />
+                    </button>
+                </div>
+                <button
+                    className="block sm:hidden"
+                    onClick={() => setIsOpen((prev) => !prev)}
+                >
+                    <Menu />
+                </button>
             </div>
-            <div>
-                <ul className='flex gap-8'>
-                    <li>Project</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div className='flex gap-8'>
-                <button>Resume</button>
-                <button>Hire Me</button>
+            <div
+                className={`${isOpen ? "h-full overflow-auto" : "h-0 overflow-hidden"
+                    } w-full transition-all fixed`}
+            >
+                <nav className="block sm:hidden w-50 ml-auto px-3 py-2 rounded-xl bg-bg-light border space-y-3">
+                    {navItems.map(item => (
+                        <div className={`flex gap-2 px-4 py-2 rounded-xl text-sm text-text-primary ${active === item.id ? "bg-bg-dark font-semibold shadow-msm" : "bg-bg-light"} transition-all`}
+                            onClick={() => scrollToSection(item.id)}
+                            key={item.id}
+                        >
+                            {item.icon} <span>{item.label}</span>
+                        </div>
+                    ))}
+                    <div className="mt-2 border-t border-border/30 px-2 pt-3 space-x-4">
+                        <button className="hover:shadow-msm p-1.5 transition-all duration-300 rounded-md">
+                            <span
+                                className="size-6 inline-block"
+                                dangerouslySetInnerHTML={{ __html: siGithub.svg }}
+                            />
+                        </button>
+                        <button className="hover:shadow-msm p-1.5 transition-all duration-300 rounded-md">
+                            <img className="size-6" src="./linkedin.svg" />
+                        </button>
+                    </div>
+                </nav>
             </div>
         </header>
-    )
+    );
 }
-
-export default Header
